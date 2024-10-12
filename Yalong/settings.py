38d16f750e91694f6b7f63dd29c77916043d82d5
Danikha98 <danikhayalong@gmail.com>
2024-10-12 23:38:13 +0800

    commit

--- Yalong/settings.py
+++ Yalong/settings.py
@@ -38,7 +38,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'Arcenal.urls'
+ROOT_URLCONF = 'Yalong.urls'
 
 TEMPLATES = [
     {
@@ -56,7 +56,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'Arcenal.wsgi.application'
+WSGI_APPLICATION = 'Yalong.wsgi.application'
 
 
 # Database
